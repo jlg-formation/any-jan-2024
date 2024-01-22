033ce02cc02320987a4f5d1f1159ac764df4d47a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-22 14:34:46 +0100

    dev: added serve-index

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 console.log("About to start a server...");
 
 const express = require("express");
+const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
 const publicDir = ".";
@@ -11,6 +12,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
