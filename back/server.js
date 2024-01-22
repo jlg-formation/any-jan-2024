e42e7fa822e4097fdbf29e7acf80ad3954bbd5de
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-22 14:31:56 +0100

    dev: added static server

--- back/server.js
+++ back/server.js
@@ -3,10 +3,14 @@ console.log("About to start a server...");
 const express = require("express");
 const app = express();
 const port = 3000;
+const publicDir = ".";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req: ", req.method, req.url);
+  next();
 });
+
+app.use(express.static(publicDir));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
