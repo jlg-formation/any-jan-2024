aa20aadaa2fff5e3433e4c8afbaf242b748346b4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-23 15:53:54 +0100

    dev: added > in home button

--- front/src/app/routes/home/home.component.ts
+++ front/src/app/routes/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  faAngleRight = faAngleRight;
+}
