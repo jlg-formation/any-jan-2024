392fd8fdeaa8965175bf22c345532a64da754d40
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-23 16:19:04 +0100

    dev: added fontawesome icons

--- front/src/app/routes/list/list.component.ts
+++ front/src/app/routes/list/list.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faPlus,
+  faRotateRight,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+  faRotateRight = faRotateRight;
+  faPlus = faPlus;
+  faTrashAlt = faTrashAlt;
+}
