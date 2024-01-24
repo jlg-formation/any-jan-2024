77e09bef3dffed519e190af2f2b1afea7366a1a6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-24 14:19:26 +0100

    dev: don't be aggressive in form

--- front/src/app/routes/add/add.component.ts
+++ front/src/app/routes/add/add.component.ts
@@ -1,14 +1,26 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss',
 })
 export class AddComponent {
   faPlus = faPlus;
+  f = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    qty: new FormControl(1, [Validators.required, Validators.min(1)]),
+  });
 }
